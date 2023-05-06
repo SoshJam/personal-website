56a99f667028f91a3a316aa3ba7f392fa113616b
Sam Hill <soshjam@soshjam.com>
2023-05-05 22:32:26 -0600

    Navbar and page layout

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.scss';
+import NavLink from './NavLink';
+
+export const metadata = {
+    title: 'Sam Hill Web Development',
+    description: 'Quality web development services with a focus on eCommerce.',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+
+    return (
+        <html lang="en">
+            <body>
+
+                <header>
+                    <h1>Sam Hill Web Development</h1>
+                    <nav>
+                        <ul>
+                            <NavLink href="/">Home</NavLink>
+                            <NavLink href="/about">About Me</NavLink>
+                            <NavLink href="/projects">Projects</NavLink>
+                            <NavLink href="/contact">Contact</NavLink>
+                        </ul>
+                    </nav>
+                </header>
+
+                {children}
+
+            </body>
+        </html>
+    );
+};
