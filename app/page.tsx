746be76e4cb52c313cc763340a3649aebcb96418
Sam Hill <soshjam@soshjam.com>
2023-05-05 22:32:58 -0600

    Default homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+        <h1>Sam Hill Web Development</h1>
+        <p>Quality web development services with a focus on eCommerce.</p>
+    </main>
+  );
+}
